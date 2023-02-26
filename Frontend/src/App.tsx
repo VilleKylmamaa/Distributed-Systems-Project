d4412688d552a8615fe82a40ef3fa5bc1044989a
VilleKylmamaa <ville.kylmamaa@gmail.com>
2023-02-26 11:54:44 +0200

    Improved state handling in frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -24,6 +24,8 @@ function App() {
   const [ username, setUsername ] = useState<string>("");
   const [ roomName, setRoomName ] = useState<string>("");
   const [ isConnecting, setIsConnecting ] = useState<boolean>(false);
+  const [ connectingMessage, setConnectingMessage ] = useState<string>("");
+  const [ errorMessage, setErrorMessage ] = useState<string>("");
 
   const fetchAppServerUrlFromLoadBalancer = async(urlIndex: number) : Promise<string> => {
     try {
@@ -40,20 +42,38 @@ function App() {
   }
 
   const joinRoom = async () : Promise<void> => {
-    setIsConnecting(true);
+    setErrorMessage("")
+    setIsConnecting(true)
+    let signalrUrl = "";
+    let urlIndex = 0;
+    let connectingMessageTimeout = 0
+
     try {
-      let signalrUrl = "";
-      let urlIndex = 0;
-      while (signalrUrl === "" && urlIndex < 6) {
-        signalrUrl = await fetchAppServerUrlFromLoadBalancer(urlIndex % 3);
+      while (signalrUrl === "" && urlIndex < 9) {
+        const urlIndexModulo = urlIndex % 3;
+        // Delay first connection message to avoid flashing in the case of immediate connection
+        if (urlIndex === 0) {
+          connectingMessageTimeout = setTimeout(() => {
+            setConnectingMessage(`Fetching application server url from ${loadBalancerUrls[urlIndexModulo]}`);
+          }, 200)
+        } else{
+          setConnectingMessage(`Fetching application server url from ${loadBalancerUrls[urlIndexModulo]}`);
+        }
+
+        signalrUrl = await fetchAppServerUrlFromLoadBalancer(urlIndexModulo);
         urlIndex++;
+
         if (urlIndex === 9) {
           console.log("Attempted to fetch application server url from load balancer "
             + urlIndex + " times. Giving up.")
-          setIsConnecting(false);
+          clearTimeout(connectingMessageTimeout)
+          setIsConnecting(false)
+          setErrorMessage("Error: Failed to fetch application server url from load balancer.")
+          setConnectingMessage("")
           return
         }
       }
+      clearTimeout(connectingMessageTimeout)
 
       const hubConnection = new HubConnectionBuilder()
         .withUrl(signalrUrl)
@@ -77,9 +97,13 @@ function App() {
 
       setConnection(hubConnection)
       setRoomJoined(true)
+      setIsConnecting(false)
+      setConnectingMessage("")
     }
     catch (error) {
       console.error(error)
+      setIsConnecting(false)
+      setConnectingMessage("")
     }
   }
 
@@ -112,14 +136,15 @@ function App() {
           />
           <button
             onClick={joinRoom}
-            disabled={!roomName}
+            disabled={!roomName || isConnecting}
           >
             Join room
-            </button>
+          </button>
 
-          {isConnecting && (
-            <div>Connecting...</div>
-          )}
+          <div className="status-container">
+            {isConnecting && <div className="connecting-message">{connectingMessage}</div>}
+            {errorMessage !== "" && <div className="error-message">{errorMessage}</div>}
+          </div>
         </div>
       )}
 
